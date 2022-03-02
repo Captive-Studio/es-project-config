5cfe7022c3a277be2b0e26b8cfe11967fbcb84ae
Julien Polo <julien.polo@captive.fr>
2022-03-02 15:42:08 +0100

    chore(lint): correct linter errors

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,4 +1,4 @@
 // Examples of nice configurations
 // @see https://github.com/fisker/shared-configs/tree/main/packages/stylelint-config
 
-module.exports = require('./rules.js');
+module.exports = require('./rules');
