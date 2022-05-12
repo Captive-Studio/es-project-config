ce26409cd7b2cd65b3ee18e316c4bf0cd66d40e6
Julien Polo <julien.polo@captive.fr>
2022-05-12 11:58:07 +0200

    fix: disable no-descending-specificity rule

--- packages/stylelint-config/rules.js
+++ packages/stylelint-config/rules.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
   plugins: ['stylelint-order', 'stylelint-scss', 'stylelint-config-rational-order/plugin'],
   rules: {
+    'no-descending-specificity': null, // Many false positive https://github.com/stylelint/stylelint/issues/3516
     'selector-class-pattern': null, // TODO: enforce BEM naming convention
   },
 };
