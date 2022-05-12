823b58c49b79b3514e9c13baa56503d6b8e1e555
Julien Polo <julien.polo@captive.fr>
2022-05-12 11:27:43 +0200

    fix: cleanup stylelint rules

--- packages/stylelint-config/rules.js
+++ packages/stylelint-config/rules.js
@@ -2,15 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
   plugins: ['stylelint-order', 'stylelint-scss', 'stylelint-config-rational-order/plugin'],
   rules: {
-    'at-rule-no-unknown': null,
-    'order/properties-order': [],
-    'plugin/rational-order': [true],
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['layer'],
-      },
-    ],
-    'selector-class-pattern': null,
+    'selector-class-pattern': null, // TODO: enforce BEM naming convention
   },
 };
