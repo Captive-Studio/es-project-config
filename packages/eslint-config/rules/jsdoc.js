a4595ab7f7fa9780b3ac268f4cd58c4e822a1e81
Julien Polo <julien.polo@captive.fr>
2022-05-23 11:03:21 +0200

    feat: bump jsdoc rules

--- packages/eslint-config/rules/jsdoc.js
+++ packages/eslint-config/rules/jsdoc.js
@@ -1,10 +1,16 @@
-const { off } = require('./_rule');
+const { off, error, warn } = require('./_rule');
 
 module.exports = {
   extends: ['plugin:jsdoc/recommended'],
   plugins: ['jsdoc'],
   rules: {
+    'jsdoc/no-undefined-types': off, // https://github.com/gajus/eslint-plugin-jsdoc/issues/839
+    'jsdoc/require-hyphen-before-param-description': [warn, 'always'],
     'jsdoc/require-jsdoc': off,
+    'jsdoc/require-param-description': off,
+    'jsdoc/require-returns': off,
+    'jsdoc/valid-types': off, // FIXME: reports lots of false positive
+    strict: [error, 'safe'],
   },
   settings: {
     jsdoc: {
