d2c7f5eb0c0d400da4da3650274558a32c7057d3
Julien Polo <julien.polo@captive.fr>
2022-11-24 09:30:35 +0100

    ci: add some built files to repository because needed for tooling

---
+++ packages/prettier-config/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_js_1 = __importDefault(require("@w5s/prettier-config/lib/index.js"));
+const prettierConfig = {
+    ...index_js_1.default,
+};
+exports.default = prettierConfig;
+//# sourceMappingURL=index.js.map
