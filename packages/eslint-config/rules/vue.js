4bea4ef6fd5754a2cf86459b417be259b480abfe
Julien Polo <julien.polo@gmail.com>
2022-12-05 16:09:32 +0100

    ✨ Active vue/component-api-style pour script-setup seulement

--- packages/eslint-config/rules/vue.js
+++ packages/eslint-config/rules/vue.js
@@ -3,6 +3,6 @@ const { error, concatESConfig } = require('./_rule.js');
 module.exports = concatESConfig({
   extends: ['plugin:vue/vue3-recommended'],
   rules: {
-    'vue/component-api-style': error, // ['script-setup', 'composition']
+    'vue/component-api-style': [error, ['script-setup']], // Use script-setup as default syntax, can be overriden in project if needed
   },
 });
