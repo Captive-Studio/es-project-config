e55ab3072d89dbec742f8e303031e0a5d008b0c5
Julien Polo <julien.polo@gmail.com>
2023-01-31 10:43:55 +0100

    🐛 Enable vue parsing

--- packages/eslint-config/lib/es.js
+++ packages/eslint-config/lib/es.js
@@ -3,5 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const es_js_1 = __importDefault(require("@w5s/eslint-config/lib/es.js"));
-const config = es_js_1.default;
+const dev_1 = require("@w5s/dev");
+const config = dev_1.ESLintConfig.concat({
+    parserOptions: {
+        extraFileExtensions: ['.vue'],
+    },
+}, es_js_1.default);
 module.exports = config;
--- packages/eslint-config/src/es.ts
+++ packages/eslint-config/src/es.ts
@@ -1,6 +1,14 @@
 import type eslint from 'eslint';
 import parentConfig from '@w5s/eslint-config/lib/es.js';
+import { ESLintConfig } from '@w5s/dev';
 
-const config: eslint.Linter.Config = parentConfig;
+const config: eslint.Linter.Config = ESLintConfig.concat(
+  {
+    parserOptions: {
+      extraFileExtensions: ['.vue'],
+    },
+  },
+  parentConfig
+);
 
 export = config;
