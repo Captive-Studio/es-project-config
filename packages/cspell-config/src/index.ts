93410a3422fefff4cb92f37a937323d2df19f10d
Julien Polo <julien.polo@gmail.com>
2023-02-28 21:06:12 +0100

    ✨ Add cspell package

---
+++ packages/cspell-config/src/index.ts
@@ -0,0 +1,34 @@
+import type { AdvancedCSpellSettings } from '@cspell/cspell-types';
+import cSpellSettings from '@w5s/cspell-config/lib/index.js';
+
+const toArray = <T>(value: T | T[] | undefined) => (Array.isArray(value) ? value : value == null ? [] : [value]);
+const defaultSettings = cSpellSettings;
+const settings: AdvancedCSpellSettings = {
+  ...defaultSettings,
+  name: 'Captive default settings .js',
+  id: 'captive-default-js',
+  language: 'en,fr',
+  description: 'Default cspell configuration.',
+  dictionaryDefinitions: [
+    // ...toArray(defaultSettings.dictionaryDefinitions),
+    {
+      addWords: false,
+      name: 'captive-filetypes',
+      path: '../dict/filetypes.txt',
+      description: 'Additional file types',
+    },
+    {
+      addWords: false,
+      name: 'captive-names',
+      path: '../dict/names.txt',
+      description: 'Known names',
+    },
+  ],
+  dictionaries: [...toArray(defaultSettings.dictionaries), 'captive-filetypes', 'captive-names'],
+  ignoreWords: [],
+  import: [...toArray(defaultSettings.import), '@w5s/cspell-config/cspell-ext.json'],
+  ignorePaths: [...toArray(defaultSettings.ignorePaths)],
+  languageSettings: [...toArray(defaultSettings.languageSettings)],
+};
+
+export = settings;
