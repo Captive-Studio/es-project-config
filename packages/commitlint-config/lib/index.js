9192b79ef7a759e68d57ec2457d1bbe4c120beac
Julien Polo <julien.polo@captive.fr>
2023-04-19 18:05:47 +0200

    ✨ Increase header limit length

--- packages/commitlint-config/lib/index.js
+++ packages/commitlint-config/lib/index.js
@@ -3,8 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/naming-convention */
 const commitlint_config_1 = __importDefault(require("@w5s/commitlint-config"));
+const types_1 = require("@commitlint/types");
 const config = {
     ...commitlint_config_1.default,
+    rules: {
+        ...commitlint_config_1.default.rules,
+        'header-max-length': [types_1.RuleConfigSeverity.Error, 'always', 100],
+    },
 };
 exports.default = config;
--- packages/commitlint-config/src/index.ts
+++ packages/commitlint-config/src/index.ts
@@ -1,7 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/naming-convention */
 import defaultConfig from '@w5s/commitlint-config';
+import { RuleConfigSeverity } from '@commitlint/types';
 
 const config = {
   ...defaultConfig,
+  rules: {
+    ...defaultConfig.rules,
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 100],
+  },
 };
 
 export default config;
