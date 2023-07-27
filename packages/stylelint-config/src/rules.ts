86b7241d620257534b6606c8b9c57b7a44fab2a3
Julien Polo <julien.polo@captive.fr>
2023-07-27 12:15:22 +0200

    ♻️ Simplify prettier config loading

--- packages/stylelint-config/src/rules.ts
+++ packages/stylelint-config/src/rules.ts
@@ -1,44 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable global-require */
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
 
-import type * as prettier from 'prettier';
 import type { Config } from 'stylelint';
-
-const getPackageScope = (): string | undefined => {
-  try {
-    const { name } = require('../package.json') as { name?: string };
-    const prefixMatch = (name ?? '').match(/(@\w+)\//);
-    const packageScope = prefixMatch == null ? undefined : prefixMatch[1];
-    return packageScope;
-  } catch (error_: unknown) {
-    // eslint-disable-next-line no-console
-    console.warn(error_);
-
-    return undefined;
-  }
-};
-
-const getPrettierConfig = (moduleName: string): prettier.Config | undefined => {
-  try {
-    const moduleConfig = require(moduleName);
-    return moduleConfig as prettier.Config | undefined;
-  } catch {
-    return undefined;
-  }
-};
-
-// Try require '@my-organization/prettier-config'
-const getPrettierConfigDefault = () => {
-  const defaultConfig: prettier.Config = {
-    trailingComma: 'es5',
-  };
-  const packageScope = getPackageScope();
-  return (packageScope == null ? undefined : getPrettierConfig(`${packageScope}/prettier-config`)) ?? defaultConfig;
-};
+import prettierConfig from '@captive/prettier-config';
 
 const bemSelector = (/** @type {'any'|'kebabCase'|'pascalCase'} */ selector = 'any') => {
   const kebabCase = '[\\da-z-]+';
@@ -69,7 +32,7 @@ const stylelintConfig: Config = {
   reportNeedlessDisables: true,
   rules: {
     'no-descending-specificity': null, // Many false positive https://github.com/stylelint/stylelint/issues/3516
-    'prettier/prettier': [true, getPrettierConfigDefault()],
+    'prettier/prettier': [true, prettierConfig],
     'selector-class-pattern': [
       bemSelector('any'),
       {
