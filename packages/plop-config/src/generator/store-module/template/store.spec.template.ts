8ca27d8ea768948b25c58331fc1eead590077c98
Julien Polo <julien.polo@captive.fr>
2023-11-07 16:55:51 +0100

    🐛 Fix pinia initialization

--- packages/plop-config/src/generator/store-module/template/store.spec.template.ts
+++ packages/plop-config/src/generator/store-module/template/store.spec.template.ts
@@ -1,11 +1,22 @@
 import * as Variables from './variables.js';
 
 export const template = `
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { setActivePinia, createPinia } from 'pinia';
-import { use{{pascalCase ${Variables.storeName} }}Store } from './store.js';
+import { use{{pascalCase ${Variables.storeName} }}Store as useSubjectStore } from './store.js';
 
 describe('store/{{ ${Variables.storeName} }}', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe('()', () => {
+    it('constructs an object', async () => {
+      const store = useSubjectStore();
+      expect(store).toBeTypeOf('object');
+    });
+  });
+
   describe('#myMethod', () => {
     it('is dummy test', async () => {
       expect(true).toBe(true);
