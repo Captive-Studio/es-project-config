cdcaef122989daefd04eb9fa4974ad8043c45543
Julien Polo <julien.polo@captive.fr>
2023-11-09 14:13:20 +0100

    🐛 Fix minor glitches on vue component generator

--- packages/plop-config/src/generator/vue-component/template/Component.vue.template.ts
+++ packages/plop-config/src/generator/vue-component/template/Component.vue.template.ts
@@ -2,23 +2,26 @@ import * as Variables from './variables.js';
 
 export const template = `
 <script lang="ts" setup>
-import { defineOptions, withDefaults } from 'vue';
+import { defineOptions } from 'vue';
 import './{{ ${Variables.componentName} }}.scss';
 
+// Name and options
 const componentName = '{{ ${Variables.componentName} }}';
 
 defineOptions({
   name: componentName,
 });
 
-interface Props {
+// Component properties
+export interface {{ ${Variables.componentName} }}Props {
   dark?: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const props = withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<{{ ${Variables.componentName} }}Props>(), {
   dark: false,
 });
+
 </script>
 
 <template>
