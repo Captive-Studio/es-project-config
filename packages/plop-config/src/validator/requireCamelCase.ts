b28716d2be95f56c09a6faeca9b06f70757ba892
Julien Polo <julien.polo@captive.fr>
2024-01-04 15:07:02 +0100

    🐛 Fix camecase helper

--- packages/plop-config/src/validator/requireCamelCase.ts
+++ packages/plop-config/src/validator/requireCamelCase.ts
@@ -1,6 +1,6 @@
 import type { PlopValidator } from './validator.js';
 
-const camelCaseRegex = /^[a-z][A-Za-z]*$/u;
+const camelCaseRegex = /^[a-z][A-Za-z0-9]*$/u;
 
 export function requireCamelCase(): PlopValidator<unknown> {
   return (promptValue) => (camelCaseRegex.test(String(promptValue)) ? true : `${promptValue} must be camelCase`);
--- packages/plop-config/src/validator/requirePascalCase.ts
+++ packages/plop-config/src/validator/requirePascalCase.ts
@@ -1,6 +1,6 @@
 import type { PlopValidator } from './validator.js';
 
-const pascalCaseRegex = /^[A-Z][A-Za-z]*$/u;
+const pascalCaseRegex = /^[A-Z][A-Za-z0-9]*$/u;
 
 export function requirePascalCase(): PlopValidator<unknown> {
   return (promptValue) => (pascalCaseRegex.test(String(promptValue)) ? true : `${promptValue} must be PascalCase`);
