a2955299bd210af09898afb7c9e7869860ffc893
Julien Polo <julien.polo@captive.fr>
2024-04-17 18:28:18 +0200

    🐛 Add className from props

--- packages/plop-config/src/generator/react-component/template/Component.tsx.template.ts
+++ packages/plop-config/src/generator/react-component/template/Component.tsx.template.ts
@@ -9,12 +9,12 @@ const componentName = '{{ ${Variables.componentName} }}';
 
 // Component properties
 export interface {{ ${Variables.componentName} }}Props extends React.ComponentProps<'div'> {
-  example?: string
+  // example?: string
 }
 
 export function {{ ${Variables.componentName} }}(props: {{ ${Variables.componentName} }}Props) {
   return (
-    <div className={classNames(componentName)}>
+    <div className={classNames(componentName, props.className)}>
       <div className={classNames(\`$\{componentName}__Content\`)}>
         {props.example}
       </div>
