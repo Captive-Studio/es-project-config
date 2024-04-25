55c8cbedde28a00e0b8d725664ef931b76838900
Julien Polo <julien.polo@captive.fr>
2024-04-25 12:48:50 +0200

    ✨ Export Props in index.ts

--- packages/plop-config/src/generator/vue-component/template/index.template.ts
+++ packages/plop-config/src/generator/vue-component/template/index.template.ts
@@ -2,4 +2,5 @@ import * as Variables from './variables.js';
 
 export const template = `
 export { default as {{ ${Variables.componentName} }} } from './{{ ${Variables.componentName} }}.vue';
+export type { {{ ${Variables.componentName} }}Props } from './{{ ${Variables.componentName} }}.vue';
 `.slice(1);
