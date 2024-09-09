7fe772df762d5645d04c95852116f986f0027b5f
Julien Polo <julien.polo@captive.fr>
2024-09-09 13:57:17 +0200

    🔨 Restrict some path for mardown generation

--- markdown.mjs
+++ markdown.mjs
@@ -1,11 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// import path from 'node:path';
 import markdownMagic from 'markdown-magic';
 import markdownPackageJSON from 'markdown-magic-package-json';
 import markdownList from 'markdown-magic-subpackage-list';
 
-// const markdownPath = path.join(__dirname, 'README.md');
-markdownMagic(['**/*.md', '!node_modules/**', '!packages/**'], {
+markdownMagic([`*.md`, `docs/**/*.md`, '!node_modules/**', '!packages/**'], {
   transforms: {
     PKG_JSON: markdownPackageJSON,
     SUB_PACKAGE_LIST: markdownList,
